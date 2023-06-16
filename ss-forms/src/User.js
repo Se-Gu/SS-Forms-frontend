67589b5ff66f65f581edb46d2bc6786069654c8e
Se-Gu <serhat.gurgenyatagi@gmail.com>
2023-06-16 16:32:08 +0300

    can add responses to the forms

--- ss-forms/src/User.js
+++ ss-forms/src/User.js
@@ -1,133 +1,279 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import { toast } from "react-toastify";
 
 const User = () => {
   const [forms, setForms] = useState([]);
-  const [answers, setAnswers] = useState({});
-  const username = localStorage.getItem("username");
+  const [selectedForm, setSelectedForm] = useState("");
+  const [answers, setAnswers] = useState([]);
 
-  const getForms = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/api/getforms", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
+  // Mock data for forms
+  const mockForms = [
+    {
+      formName: "Form 1",
+      formQuestions: [
+        {
+          prompt: "Question 1",
+          type: "text",
+          options: "",
+          answers: [],
         },
-      });
+        {
+          prompt: "Question 2",
+          type: "dropdown",
+          options: "Option 1,Option 2,Option 3",
+          answers: [],
+        },
+        {
+          prompt: "Question 3",
+          type: "checkbox",
+          options: "Option A,Option B,Option C",
+          answers: [],
+        },
+      ],
+    },
+    {
+      formName: "Form 2",
+      formQuestions: [
+        {
+          prompt: "Question 4",
+          type: "numeric",
+          options: "",
+          answers: [],
+        },
+        {
+          prompt: "Question 5",
+          type: "longtext",
+          options: "",
+          answers: [],
+        },
+      ],
+    },
+  ];
 
-      const data = await response.json();
-
-      if (response.ok) {
-        // Forms retrieved successfully
-        console.log(data);
-        setForms(data);
-      } else {
-        // Error retrieving forms
-        console.error(data.msg);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
   useEffect(() => {
-    if (!localStorage.isToastShown) {
-      toast.success("Login successful, welcome " + username + "!", {
-        autoClose: 1000,
-      });
-      localStorage.setItem("isToastShown", true);
-    }
-  }, [username]);
-  useEffect(() => {
-    const newAnswers = {};
-    forms.forEach((form, i) => {
-      newAnswers[i] = {
-        title: form.title,
-        username: username,
-        answers: form.questions.map(() => ""),
-      };
-    });
-    setAnswers(newAnswers);
-  }, [forms, username]);
-
-  const handleInputChange = (formIndex, questionIndex, event) => {
-    if (answers[formIndex]) {
-      const newAnswers = { ...answers };
-      newAnswers[formIndex].answers[questionIndex] = event.target.value;
-      setAnswers(newAnswers);
-    }
-  };
-
-  const handleSubmitForm = async (formIndex) => {
-    if (answers[formIndex]) {
-      const submission = [
-        answers[formIndex].title,
-        answers[formIndex].username,
-        ...answers[formIndex].answers,
-      ];
-
+    const fetchForms = async () => {
+      const token = localStorage.getItem("token");
       try {
-        const response = await fetch("http://localhost:5000/api/submit_form", {
-          method: "POST",
+        const response = await fetch("http://localhost:5000/api/forms", {
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
           },
-          body: JSON.stringify({
-            form_title: submission[0],
-            username: submission[1],
-            answers: submission.slice(2),
-          }),
         });
 
         const data = await response.json();
 
-        if (response.ok) {
-          // Form submitted successfully
-          console.log(data.msg);
+        if (!response.ok) {
+          console.error(data);
+          return []; // Return an empty array in case of an error
         } else {
-          // Form not submitted, error occurred
-          console.error(data.msg);
+          return data; // Return the fetched data
         }
       } catch (error) {
         console.error("Error:", error);
+        return []; // Return an empty array in case of an error
       }
+    };
+
+    fetchForms().then((forms) => {
+      setForms(forms);
+      console.log(forms);
+    });
+  }, []);
+
+  const handleFormChange = (event) => {
+    const selectedFormName = event.target.value;
+    const selectedForm = forms.find(
+      (form) => form.formName === selectedFormName
+    );
+    setSelectedForm(selectedForm);
+    setAnswers(
+      selectedForm.formQuestions.map((question) => ({
+        question: question.prompt,
+        answer: [],
+        questionId: question.id,
+      }))
+    );
+  };
+
+  const handleAnswerChange = (event, index) => {
+    const updatedAnswers = [...answers];
+    updatedAnswers[index].answer = event.target.value;
+    setAnswers(updatedAnswers);
+  };
+
+  const handleCheckboxChange = (event, index, option) => {
+    const updatedAnswers = [...answers];
+    if (event.target.checked) {
+      updatedAnswers[index].answer.push(option);
+    } else {
+      const optionIndex = updatedAnswers[index].answer.indexOf(option);
+      if (optionIndex !== -1) {
+        updatedAnswers[index].answer.splice(optionIndex, 1);
+      }
+    }
+    setAnswers(updatedAnswers);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/forms/${selectedForm.id}/responses`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(answers),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        toast.success(data.msg);
+        console.log(data);
+        setTimeout(() => {
+          window.location.href = "/User";
+        }, 1000);
+      } else {
+        toast.error(data.msg);
+        console.error(data);
+      }
+    } catch (error) {
+      toast.error("There was an error filling the form");
+      console.error("Error:", error);
     }
   };
 
   return (
-    <div className="container">
-      <h1>User Panel</h1>
-      <div className="form-list-container">
-        <h1>Forms List</h1>
-        <button onClick={getForms}>Get Forms</button>
-        {forms.map((form, formIndex) => (
-          <div key={formIndex}>
-            <h2>{form.title}</h2>
-            <ul>
-              {form.questions.map((question, questionIndex) => (
-                <li key={questionIndex}>
-                  {question}
-                  <input
-                    type="text"
-                    placeholder="Your answer"
-                    value={
-                      (answers[formIndex] &&
-                        answers[formIndex].answers[questionIndex]) ||
-                      ""
-                    }
-                    onChange={(event) =>
-                      handleInputChange(formIndex, questionIndex, event)
-                    }
+    <Box sx={{ maxWidth: 600, margin: "0 auto", marginTop: "5rem" }}>
+      <Typography variant="h4" sx={{ marginBottom: "1rem" }}>
+        User Page
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <FormControl fullWidth sx={{ marginBottom: "1rem" }}>
+          <Select
+            value={selectedForm.formName || ""}
+            onChange={handleFormChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Select a form" }}
+          >
+            <MenuItem value="" disabled>
+              Select a form
+            </MenuItem>
+            {forms?.map((form) => (
+              <MenuItem key={form.formName} value={form.formName}>
+                {form.formName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {selectedForm && (
+          <div>
+            {selectedForm?.formQuestions.map((question, index) => (
+              <FormControl key={index} fullWidth sx={{ marginBottom: "1rem" }}>
+                <Typography variant="subtitle1">{question.prompt}</Typography>
+                {question?.type === "text" && (
+                  <TextField
+                    value={answers[index].answer}
+                    onChange={(e) => handleAnswerChange(e, index)}
+                    required
+                    label="Answer"
+                    fullWidth
+                    margin="normal"
                   />
-                </li>
-              ))}
-              <button onClick={() => handleSubmitForm(formIndex)}>
-                Submit
-              </button>
-            </ul>
+                )}
+                {question?.type === "longtext" && (
+                  <TextField
+                    multiline
+                    rows={4}
+                    value={answers[index].answer}
+                    onChange={(e) => handleAnswerChange(e, index)}
+                    required
+                    label="Answer"
+                    fullWidth
+                    margin="normal"
+                  />
+                )}
+                {question?.type === "numeric" && (
+                  <TextField
+                    type="number"
+                    value={answers[index].answer}
+                    onChange={(e) => handleAnswerChange(e, index)}
+                    required
+                    label="Answer"
+                    fullWidth
+                    margin="normal"
+                  />
+                )}
+                {question?.type === "dropdown" && (
+                  <FormControl fullWidth>
+                    <Select
+                      value={answers[index].answer}
+                      onChange={(e) => handleAnswerChange(e, index)}
+                      displayEmpty
+                      inputProps={{ "aria-label": "Select an option" }}
+                    >
+                      <MenuItem value="" disabled>
+                        Select an option
+                      </MenuItem>
+                      {question.options?.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+                {question?.type === "checkbox" && (
+                  <FormControl fullWidth>
+                    {question.options?.map((option) => (
+                      <FormControlLabel
+                        key={option}
+                        control={
+                          <Checkbox
+                            checked={answers[index].answer.includes(option)}
+                            onChange={(e) =>
+                              handleCheckboxChange(e, index, option)
+                            }
+                          />
+                        }
+                        label={option}
+                      />
+                    ))}
+                  </FormControl>
+                )}
+              </FormControl>
+            ))}
+
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{ marginTop: "1rem" }}
+            >
+              Submit
+            </Button>
           </div>
-        ))}
-      </div>
-    </div>
+        )}
+      </form>
+    </Box>
   );
 };
 
